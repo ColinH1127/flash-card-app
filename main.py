e85032274a012c48a7890c90d38d8ec3fb0e5171
ColinH1127 <colinhussey82@gmail.com>
2025-09-27 00:34:15 -0400

    feat: add function to generate random word

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from tkinter import *
+import pandas as pd
+from pandas.core.interchange.dataframe_protocol import DataFrame
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+def generate_word():
+    word_df = pd.read_csv("./data/french_words.csv")
+    word_dict = word_df.to_dict(orient="records")
+    random_index = random.choice(range(0, len(word_dict)))
+    random_fr_word = word_dict[random_index]["French"]
+    canvas.itemconfig(word, text=random_fr_word)
+
 
 window = Tk()
 window.title("Flash Card App")
@@ -12,10 +23,10 @@ canvas.create_image(400, 260, image=card_front_img)
 title = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
 word = canvas.create_text(400, 300, text="Word", font=("Arial", 60, "bold"))
 my_image1 = PhotoImage(file="./images/right.png")
-check_button = Button(image=my_image1, highlightthickness=0)
+check_button = Button(image=my_image1, highlightthickness=0, command=generate_word)
 check_button.grid(row=1, column=1)
 my_image2 = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=my_image2, highlightthickness=0)
+wrong_button = Button(image=my_image2, highlightthickness=0, command=generate_word)
 wrong_button.grid(row=1, column=0)
 
 

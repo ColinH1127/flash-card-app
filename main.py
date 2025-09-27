ae6541b035dca02632bd3a37b80a630ff81d08bf
ColinH1127 <colinhussey82@gmail.com>
2025-09-27 00:50:11 -0400

    feat: refine word generating

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-def generate_word():
+def generate_fr_word():
     word_df = pd.read_csv("./data/french_words.csv")
     word_dict = word_df.to_dict(orient="records")
     random_index = random.choice(range(0, len(word_dict)))
     random_fr_word = word_dict[random_index]["French"]
+    canvas.itemconfig(title, text="French")
     canvas.itemconfig(word, text=random_fr_word)
 
 
@@ -20,16 +21,16 @@ canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 canvas.grid(row=0, column=0, columnspan=2)
 card_front_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 260, image=card_front_img)
-title = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
-word = canvas.create_text(400, 300, text="Word", font=("Arial", 60, "bold"))
+title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+word = canvas.create_text(400, 300, text="", font=("Arial", 60, "bold"))
 my_image1 = PhotoImage(file="./images/right.png")
-check_button = Button(image=my_image1, highlightthickness=0, command=generate_word)
+check_button = Button(image=my_image1, highlightthickness=0, command=generate_fr_word)
 check_button.grid(row=1, column=1)
 my_image2 = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=my_image2, highlightthickness=0, command=generate_word)
+wrong_button = Button(image=my_image2, highlightthickness=0, command=generate_fr_word)
 wrong_button.grid(row=1, column=0)
 
-
+generate_fr_word()
 
 
 
